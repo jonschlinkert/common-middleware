73a813415d5d6cdbf35c6a8afe806b7b06c84c2a
jonschlinkert <jon.schlinkert@sellside.com>
2016-05-27 15:32:06 -0400

    run update

--- utils.js
+++ utils.js
@@ -1,22 +1,6 @@
 'use strict';
 
-/**
- * Module dependencies
- */
-
 var utils = require('lazy-cache')(require);
-
-/**
- * Temporarily re-assign `require` to trick browserify
- * into recognizing lazy dependencies.
- *
- * This tiny bit of ugliness has the huge dual advantage of
- * only loading modules that are actually called at some
- * point in the lifecycle of the application, whilst also
- * allowing browserify and webpack to find modules that
- * are depended on but never actually called.
- */
-
 var fn = require;
 require = utils;
 
@@ -27,11 +11,6 @@ require = utils;
 require('define-property', 'define');
 require('mixin-deep', 'merge');
 require('parser-front-matter', 'matter');
-
-/**
- * Restore `require`
- */
-
 require = fn;
 
 /**
